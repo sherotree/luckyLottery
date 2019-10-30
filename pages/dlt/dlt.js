fbe2adb790c60b630168663c3f7d4aadcfd36adc
sherotree <wytreeshero@163.com>
2019-10-30 22:59:08 +0800

    dlt&ssq finished

--- pages/dlt/dlt.js
+++ pages/dlt/dlt.js
@@ -1,60 +1,48 @@
 // pages/dlt/dlt.js
+const app = getApp();
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    lotteryList: [],
+    page: 1
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this._loadMore();
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
+  _loadMore: function () {
+    const that = this;
+    wx.request({
+      url: `https://apis.juhe.cn/lottery/history?key=96ba11ab9a0c841e566060163bcd1128&lottery_id=dlt&page_size=8&page=${that.data.page}`,
+      success: function (res) {
+        console.log(res.data.result)
+        that.setData({
+          lotteryList: [
+            ...that.data.lotteryList,
+            ...res.data.result.lotteryResList.map(item => ({
+              ...item,
+              lottery_res: item.lottery_res.split(','),
+              // lottery_pool_amount: (item.lottery_pool_amount.replace(/,/g, '') / Math.pow(10, 9)).toFixed(2)
+            }))
+          ],
+          page: that.data.page + 1,
+        })
+      }
+    })
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-
+    this._loadMore();
   },
 
   /**
